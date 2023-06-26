c1e80e6656b251e7551e856ba7f10fa972d06c8d
Eyüp Kağan Eyüpoğlu <72672133+hirelaxstudio@users.noreply.github.com>
2023-06-26 03:38:45 +0300

    Wall Slide ve Wall Jump mekaniği eklendi.
    Player Duvardan aşağı yavaşça kayabiliyor ve W tuşuna basınca karşı duvara addforce yardımıyla atlıyor. Wall Jumo sırasında Double Jump yapmayı kapattım çünkü doublejump animasyonu boxcollider'i döndürdüğü için duvara temasda sıkıntı çıkarıyor, bu da wall slide'ın gerçekleşmesine engel oluyor.
    
    Issue 1: Double jump yaptıktan sonra wall slide yapınca jumping animasyonuna geçiş olmuyor.
    
    Issue 2: Wall Jump sırasında Player'ın yüzü ters yöne dönük.

--- Assets/Game Assets/Scripts/PlayerMovement.cs
+++ Assets/Game Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.IO;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -12,14 +13,23 @@ public class PlayerMovement : MonoBehaviour
     private float dirx; // Yatay giriþ deðeri
     private float raycastExtentY; // Yatay raycast
     private float raycastExtentX; // Dikey raycast
-    private bool doubleJump; // Çift zýplama
-    public bool DoubleJump { get { return doubleJump; } set { doubleJump = value; } }
-
     [SerializeField] private float playerSpeed = 7f; // Oyuncunun hareket hýzý
     [SerializeField] private float playerJump = 14f; // Oyuncunun zýplama gücü
+    [SerializeField] private float wallSlideSpeed = 2f; // Duvara kayma hýzý
+    [SerializeField] private float wallJumpForce = 10f; // Duvara zýplama gücü
+
+    private int isWall; // Duvara çarpýp çarpmadýðýný kontrol etmek için kullanýlan int
+
+    private bool isGround; // Yerde olup olmadýðýný kontrol etmek için kullanýlan bool
+    private bool isWallSliding; // Duvara kayýp kaymadýðýný kontrol etmek için kullanýlan bool
+    private bool doubleJump; // Çift zýplama
+    private bool iswallJump;
+
+    public bool DoubleJump { get { return doubleJump; } set { doubleJump = value; } } // doubleJump deðiþkenine eriþim ve deðiþtirme
+
     [SerializeField] private LayerMask groundLayer; // Yerde olup olmadýðýný kontrol etmek için kullanýlan layer maskesi
 
-    private enum MovementState { idle, running, jumping, falling, doubleJump } // Oyuncunun hareket durumlarý
+    private enum MovementState { idle, running, jumping, falling, doubleJump, wallSlide } // Oyuncunun hareket durumlarý
 
     private void Awake()
     {
@@ -36,47 +46,71 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        bool isGrounded = IsGroundedCheck(); // Oyuncunun yerde olup olmadýðýný kontrol et
+        isGround = IsGroundedCheck(); // Oyuncunun yerde olup olmadýðýný kontrol et
+        isWall = IsWallCheck(); // Oyuncunun duvara çarpýp çarpmadýðýný kontrol et
 
         dirx = Input.GetAxisRaw("Horizontal"); // Yatay giriþ deðerini al
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-            if (isGrounded)
+            // Burada kaldýn. walljump yaparken doublejump yapmamasýný saðlaman lazým.
+            if (isWallSliding)
             {
-                Debug.Log("doubleJump: True");
+                iswallJump = true;
+                rb.velocity = new Vector2(rb.velocity.x, playerJump);
+                rb.AddForce(new Vector2(-isWall * wallJumpForce, 0), ForceMode2D.Impulse);
+            }
+            else if (isGround)
+            {
+                iswallJump = false;
                 rb.velocity = new Vector2(rb.velocity.x, playerJump);
                 doubleJump = true;
             }
-            else if (doubleJump)
+            else if (doubleJump && !iswallJump)
             {
-                Debug.Log("doubleJump: False");
                 rb.velocity = new Vector2(rb.velocity.x, playerJump * 0.75f);
                 doubleJump = false;
             }
         }
+
+        WallSlide(); // Duvara kayma
 
         UpdateAnimationState(); // Animasyon durumunu güncelle
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(dirx * playerSpeed, rb.velocity.y); // Hareket etmek için hýzý güncelle
+        if (isGround || dirx != 0)
+        {
+            rb.velocity = new Vector2(dirx * playerSpeed, rb.velocity.y); // Hareket etmek için hýzý güncelle
+        }
+        
     }
 
+    // Oyuncu doublejump yaptýktan sonra duvarda kayarsa jumping animasyonuna geçiþ olmuyor.
     private void UpdateAnimationState()
     {
         MovementState state = MovementState.idle; // Hareket durumunu varsayýlan olarak idle olarak ayarla
 
-        
-        if (rb.velocity.y > .1f && doubleJump) // Yükselme hýzý pozitif ise (zýplama animasyonu)
+        if (isWallSliding) // Duvara kayýyorsa (duvara kayma animasyonu)
         {
-            Debug.Log("Jump Anim");
+            if (isWall == 1)
+            {
+                sr.flipX = false;
+                state = MovementState.wallSlide;
+            }
+            else if (isWall == -1)
+            {
+                sr.flipX = true;
+                state = MovementState.wallSlide;
+            }
+        }
+        else if (rb.velocity.y > .1f && doubleJump) // Yükselme hýzý pozitif ise (zýplama animasyonu)
+        {
             state = MovementState.jumping;
         }
         else if (rb.velocity.y > .1f && !doubleJump)
         {
-            Debug.Log("Double Jump Anim");
             state = MovementState.doubleJump;
         }
         else if (rb.velocity.y < -.1f) // Yükselme hýzý negatif ise (düþme animasyonu)
@@ -98,9 +132,29 @@ public class PlayerMovement : MonoBehaviour
         anim.SetInteger("state", (int)state); // Animator'a hareket durumunu ileti
     }
 
+    private void WallSlide()
+    {
+        int wallCheckResult = isWall;
+        if ((wallCheckResult == 1 || wallCheckResult == -1) && !isGround && rb.velocity.y < 0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private Vector2 CalculateRaycastOrigin()
+    {
+        return bc.bounds.center;
+    }
+
     private bool IsGroundedCheck()
     {
-        Vector2 raycastOrigin = bc.bounds.center; // Raycast'in baþlangýç noktasý
+        Vector2 raycastOrigin = CalculateRaycastOrigin();
+
         Vector2 raycastOriginLeft = new Vector2(raycastOrigin.x - raycastExtentX, raycastOrigin.y); // Sol raycast baþlangýç noktasý
         Vector2 raycastOriginRight = new Vector2(raycastOrigin.x + raycastExtentX, raycastOrigin.y); // Sað raycast baþlangýç noktasý
 
@@ -113,6 +167,36 @@ public class PlayerMovement : MonoBehaviour
 
         return hitLeft.collider != null || hitRight.collider != null; // Sol veya sað raycast'ten herhangi biri yerde bir þey algýladýysa true dön, aksi halde false dön
     }
+
+    private int IsWallCheck()
+    {
+        Vector2 raycastOrigin = CalculateRaycastOrigin();
+
+
+        RaycastHit2D hitLeft = Physics2D.Raycast(raycastOrigin, Vector2.left, raycastExtentX + 0.1f, groundLayer);
+        RaycastHit2D hitRight = Physics2D.Raycast(raycastOrigin, Vector2.right, raycastExtentX + 0.1f, groundLayer);
+
+        // Debug çizgilerini çiz
+        Debug.DrawRay(raycastOrigin, Vector2.left * (raycastExtentX + 0.1f), hitLeft.collider != null ? Color.green : Color.red);
+        Debug.DrawRay(raycastOrigin, Vector2.right * (raycastExtentX + 0.1f), hitRight.collider != null ? Color.green : Color.red);
+
+        if (hitLeft.collider != null)
+        {
+            Debug.Log("Hit Left");
+            return -1;
+        }
+        else if (hitRight.collider != null)
+        {
+            Debug.Log("Hit Right");
+            return 1;
+        }
+        else
+        {
+            Debug.Log("Hit None");
+            return 0;
+        }
+    }
+
 
     private IEnumerator LevelStart()
     {
